d8f6e222d8e8842367f6a2d4ab4e40443531ca13
alex44 <sergeyevnasa@gmail.com>
2026-05-18 07:09:29 +0300

    aded :bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 07:01:11 by asobolev          #+#    #+#             */
+/*   Updated: 2026/05/18 07:06:27 by asobolev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*cleanup_error(char *buff, char **storage)
+{
+	free(buff);
+	free(*storage);
+	*storage = NULL;
+	return (NULL);
+}
+
+static char	*return_remaining_storage(char *buff, char **storage)
+{
+	char	*tmp;
+
+	free(buff);
+	if (*storage)
+	{
+		tmp = *storage;
+		*storage = NULL;
+		return (tmp);
+	}
+	return (NULL);
+}
+
+static char	*append_buffer(char *storage, char *buff)
+{
+	char	*tmp;
+
+	if (!storage)
+		return (ft_strdup(buff));
+	tmp = ft_strjoin(storage, buff);
+	free(storage);
+	return (tmp);
+}
+
+static char	*extract_line(char **storage)
+{
+	char	*tmp;
+	char	*line;
+	int		len;
+
+	tmp = *storage;
+	len = ft_strlen(tmp) + 1 ;
+	*storage = ft_substr(tmp, len, ft_strlen(tmp));
+	line = ft_substr(tmp, 0, len);
+	free(tmp);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buff;
+	int			readed;
+	static char	*storage[19876543];
+
+	if ((fd < 0) || BUFFER_SIZE < 1)
+		return (NULL);
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	while (1)
+	{
+		readed = read(fd, buff, BUFFER_SIZE);
+		if (readed == -1)
+			return (cleanup_on_error(buff, &storage[fd]));
+		if (readed == 0)
+			return (return_remaining_storage(buff, &storage[fd]));
+		buff[readed] = '\0';
+		storage[fd] = append_buffer(storage, buff);
+		if (storage && ft_strchr(storage[fd], '\n'))
+			return (extract_line(&storage[fd]));
+	}
+	return (NULL);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 06:55:37 by asobolev          #+#    #+#             */
+/*   Updated: 2026/05/18 07:04:25 by asobolev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	const char	*p = s;
+
+	while (*p++)
+		;
+	return (p - s - 1);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*start;
+	char	*ptr;
+
+	ptr = malloc(ft_strlen(s) + 1);
+	if (!ptr)
+		return (NULL);
+	start = ptr;
+	while (*s)
+	{
+		*ptr = *s;
+		ptr++;
+		s++;
+	}
+	*ptr = '\0';
+	return (start);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t		i;
+	char		*a;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	i = 0;
+	a = malloc((len + 1) * sizeof(char));
+	if (!a)
+		return (NULL);
+	while (i < len)
+	{
+		a[i] = s[start + i];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if ((unsigned char)*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	len;
+	char	*strart;
+
+	ptr = 0;
+	len = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	ptr = malloc(len);
+	if (!ptr)
+		return (NULL);
+	strart = ptr;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	return (strart);
+}
