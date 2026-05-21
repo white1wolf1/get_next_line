6825489f46fa8c456a61a12479a0537d8cc1174b
white1wolf1 <96636096+white1wolf1@users.noreply.github.com>
2026-05-22 00:33:46 +0300

    Update get_next_line_bonus.c
    last change

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*append_buffer(char **storage, char *buff)
 {
